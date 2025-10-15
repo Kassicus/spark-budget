55271bb323b2bd57f85bf004b0e99cb2c250e75d
Kason <kason.suchow@gmail.com>
2025-10-15 15:40:30 -0600

    created the xcode project

---
+++ spark-budget/spark-budgetTests/spark_budgetTests.swift
@@ -0,0 +1,16 @@
+//
+//  spark_budgetTests.swift
+//  spark-budgetTests
+//
+//  Created by Kason Suchow on 10/15/25.
+//
+
+import Testing
+
+struct spark_budgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
